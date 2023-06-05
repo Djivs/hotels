b0f73f80544e2890c375a45fc8855edc44e9a896
Dmitriy <icemcpe099@gmail.com>
2023-06-06 00:06:22 +0300

    load adminwidget

--- hotels/workerswidget.cpp
+++ hotels/workerswidget.cpp
@@ -25,18 +25,25 @@ void WorkersWidget::setupUi() {
 
     model->setColumnCount(header.size());
     model->setHorizontalHeaderLabels(header);
+    
+    add = new QPushButton("Добавить новую запись");
+
+    connect(add, &QPushButton::clicked, this, &WorkersWidget::addNewWorker);
 
     save = new QPushButton("Сохранить изменения");
+
     leave = new QPushButton("Обратно");
 
     layout = new QVBoxLayout;
     layout->addWidget(new QLabel("Работники"));
     layout->addWidget(table);
+    layout->addWidget(add);
     layout->addWidget(save);
     layout->addWidget(leave);
 
     setLayout(layout);
 
+    connect(save, &QPushButton::clicked, this, &WorkersWidget::saveWorkers);
     connect(leave, &QPushButton::clicked, this, [this] {emit exit();});
 }
 
@@ -45,6 +52,9 @@ void WorkersWidget::setupWorker() {
     connect(worker, &SQLWorker::getHotelsReady, this, &WorkersWidget::processHotels);
     connect(this, &WorkersWidget::getWorkers, worker, &SQLWorker::getWorkers);
     connect(worker, &SQLWorker::getWorkersReady, this, &WorkersWidget::processWorkersData);
+
+    connect(this, &WorkersWidget::updateWorker, worker, &SQLWorker::updateWorker);
+    connect(this, &WorkersWidget::insertWorker, worker, &SQLWorker::insertWorker);
 }
 
 void WorkersWidget::loadPage() {
@@ -56,6 +66,8 @@ void WorkersWidget::processHotels(QStringList _hotels) {
     hotels = _hotels;
 }
 void WorkersWidget::processWorkersData(QVector<QVariantMap> workers) {
+    workersAmount = workers.size();
+    
     model->removeRows(0, model->rowCount());
     model->setRowCount(workers.size());
 
@@ -78,4 +90,36 @@ void WorkersWidget::processWorkersData(QVector<QVariantMap> workers) {
         model->setData(model->index(i, 2), workers[i]["position"].toString());
     }
 
+}
+
+void WorkersWidget::addNewWorker() {
+    model->insertRow(model->rowCount());
+
+    QComboBox *box = new QComboBox;
+    for (auto &i :hotels) {
+        box->addItem(i);
+    }
+
+    table->setIndexWidget(model->index(model->rowCount() - 1, 0), box);
+}
+
+void WorkersWidget::saveWorkers() {
+    for (int i = 0;i < workersAmount; ++i) {
+        emit updateWorker(getWorkersMap(i));
+    }
+    for (int i = workersAmount; i < model->rowCount(); ++i) {
+        emit insertWorker(getWorkersMap(i));
+    }
+}
+
+QVariantMap WorkersWidget::getWorkersMap(int row) {
+    QVariantMap worker;
+
+    worker["hotel"] = dynamic_cast<QComboBox*>(table->indexWidget(model->index(row, 0)))->currentText();
+    worker["name"] = model->data(model->index(row, 1));
+    worker["position"] = model->data(model->index(row, 2));
+    worker["id"] = row - 1;
+
+
+    return worker;
 }
